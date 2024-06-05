cf1cb1e8e7fddcabe7f6fae056b0c15761d14c76
juan pablo castiblanco <101476957+juanpa1999@users.noreply.github.com>
2024-06-05 17:27:53 -0500

    Update docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -6,6 +6,12 @@ echo -e "\n\n"
 
 sleep 10
 
+echo "Executing: docker exec -it back_cont alembic init alembic"
+docker exec -it back_cont alembic init alembic
+echo -e "\n\n"
+
+sleep 10
+
 echo "Executing: docker compose run --rm back alembic upgrade head"
 docker compose run --rm back alembic upgrade head
 echo -e "\n\n"
